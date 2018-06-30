425d93303da7e448ebf3281726801002b6dc1f42
Vitalii <avalor1991@gmail.com>
2018-06-30 13:33:35 -0400

    update project

--- assets/javascript/form2.js
+++ assets/javascript/form2.js
@@ -55,14 +55,24 @@ $("nav a, a[href='#home']").on('click', function(event) {
         $('html, body').animate({
             scrollTop: $(hash).offset().top
         }, 500, function() {
-
             // Add hash (#) to URL when done scrolling (default click behavior)
-            window.location.hash = hash;
-
-                                    
+            window.location.hash = hash;                          
         });
     }
 });
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBNZEuVLMqW1ShLVcaqAl56qekPAMEorgg",
+  authDomain: "bandana-30ebf.firebaseapp.com",
+  databaseURL: "https://bandana-30ebf.firebaseio.com",
+  projectId: "bandana-30ebf",
+  storageBucket: "",
+  messagingSenderId: "216379677416"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+
 // Value of input fields
 var artistInput;
 var locationInput;
@@ -84,10 +94,17 @@ var artistFB;
 var artistImage;
 // EVENTS AJAX REQUEST
 var events = [];
+var eventsByLocation = [];
+var eventIndex = 0;
+var count = 0;
+
+vex.defaultOptions.className = 'vex-theme-flat-attack';
 
 
 // AJAX calls to the Bands in Town API
 function searchBandana() {
+  eventIndex = 0;
+  events.length = 0;
 
     $.ajax({
       url: queryURLArtist,
@@ -100,13 +117,17 @@ function searchBandana() {
         promiseArtistStatus = true;
         artistName = response.name;
         artistFB = response.facebook_page_url;
-        
-        
         artistImage = response.image_url;
+        database.ref().push({
+          artistName: artistName,
+          artistImage: artistImage,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
       
       // Else let the user know the artist name couldn't be found
       } else {
-        alert("Sorry, not artist found")
+        vex.dialog.alert("Sorry, not artist found");
+
       };
 
       // Only when both promises came back, generate the cards
@@ -127,84 +148,124 @@ function searchBandana() {
         events.push(response);
         // console.log(events);
       };
-
       // Only when both promises came back, generate the cards
       if (promiseArtistStatus === true && promiseEventsStatus === true) {
         generateCards();
       };
-
     });
-
-
   }
 
-function generateCards() {
+  function generateCards() {
 
-  // *** ARTIST INFORMATION ***
-  // Dynamically creating the card
-  $("#artist-holder").empty();
-  var cardDiv = $("<div class='card' style='width: 18rem; position: relative'>");
-  var cardImg = $("<img class='card-img-top'>").attr("src", artistImage);
-  var cardBody = $("<div class='card-body d-flex justify-content-between' style='padding: 0.5rem; width: 100%; position: absolute; bottom: 0;'>");
-  if (artistFB !== "") {
-    var cardFB = $("<a href='"+ artistFB +"' target='_blank'><img src='fb_logo.png' style='width: 30px'></a>");
-  } else {
-    var cardFB = $("<div>");
-  };
-  var cardName = $("<h4 class='card-title' style='margin: 0; font-weight: bold; background-color: rgba(255, 208, 0, 0.5); color: black; padding: 0.5rem 1rem; border-radius: 10px;'>").text(artistName);
-  // Append the new artist content
-  cardBody.append(cardName, cardFB);
-  cardDiv.append(cardImg, cardBody);
-  $("#artist-holder").append(cardDiv);
-
-  // *** EVENTS INFORMATION ***
-  // Dynamically creating the cards
-  $("#events-holder").empty();
+    // *** ARTIST INFORMATION ***
+      // Dynamically creating the card
+      $("#artist-holder").empty();
+      var cardDiv = $("<div class='card' style='width: 18rem; position: relative'>");
+      var cardImg = $("<img class='card-img-top'>").attr("src", artistImage);
+      var cardBody = $("<div class='card-body d-flex justify-content-between' style='padding: 0.5rem; width: 100%; position: absolute; bottom: 0;'>");
+      if (artistFB !== "") {
+        var cardFB = $("<a href='"+ artistFB +"' target='_blank'><img src='assets/images/fb_logo.png' style='width: 30px'></a>");
+      } else {
+        var cardFB = $("<div>");
+      };
+      var cardName = $("<h4 class='card-title' style='margin: 0; font-weight: bold; background-color: rgba(255, 208, 0, 0.5); color: black; padding: 0.5rem 1rem; border-radius: 10px;'>").text(artistName);
+      // Append the new artist content
+      cardBody.append(cardName, cardFB);
+      cardDiv.append(cardImg, cardBody);
+      $("#artist-holder").append(cardDiv);
   
-  for (let i = 0; i < events[0].length; i++) {
-    
+    if (locationInput === "") {
+      // *** EVENTS INFORMATION ***
+      // Dynamically creating the cards
+      $("#events-holder").empty();
+      
+      for (let i = 0; i < 10; i++) {
+        
+        var eventDiv = $("<div class='card d-flex flex-nowrap'>");
+        var eventColumn1 = $("<div class='col-lg-6'>");
+        var eventColumn2 = $("<div class='col-lg-6'>");
+        eventColumn1.append("<p>" + (eventIndex+1) + "</p>")
+        var eventDate = $("<p>" + moment(events[0][i].datetime).format("MMMM D YYYY, h:mm A") + "</p>");
+        var eventLocation = $("<p>" + events[0][i].venue.city + " " + events[0][i].venue.region + " " + events[0][i].venue.country + "</p>");
+        var eventVenue = $("<p>" + events[0][i].venue.name + "</p>");
+        var eventURL = $("<a href='" + events[0][i].url + "' target='_blank'><p> Get tickets</p></a>");
+        var eventMap = $("<a href='https://www.google.com/maps/place/" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "' target='_blank'><img src='https://maps.googleapis.com/maps/api/staticmap?center=" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&zoom=14&size=400x150&maptype=roadmap&markers=color:red%7Clabel:C%7C" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&key=AIzaSyDqItaMC7jRXipvTmfaH3bzfBiCxHNflr0'></a>");        
+        eventColumn1.append(eventDate, eventLocation, eventVenue, eventURL);
+        eventColumn2.append(eventMap);
+        eventDiv.append(eventColumn1, eventColumn2);
+        $("#events-holder").append(eventDiv);
+  
+        eventIndex++;
+      };
+        
+        $("#events-holder").prepend("<button class='btn btn-danger' onclick='generateFiveMore()'>Show 5+</button>");
+        promiseEventsStatus = false;
+        promiseArtistStatus = false;
+  
+    } else {
+      
+      for (let i = 0; i < events[0].length; i++) {
+        if (locationInput === (events[0][i].venue.country).toUpperCase() || locationInput === (events[0][i].venue.city).toUpperCase() || locationInput === (events[0][i].venue.region).toUpperCase() || locationInput === (events[0][i].venue.name).toUpperCase()) {
+          eventsByLocation.push(events[0][i]);
+        };
+      };
+  
+      if (eventsByLocation.length !== 0) {
+        // *** EVENTS INFORMATION ***
+        // Dynamically creating the cards
+        $("#events-holder").empty();
+        
+        for (let i = 0; i < 10; i++) {
+          var eventDiv = $("<div class='card d-flex flex-nowrap'>");
+          var eventColumn1 = $("<div class='col-lg-6'>");
+          var eventColumn2 = $("<div class='col-lg-6'>");
+          eventColumn1.append("<p>" + (eventIndex+1) + "</p>")
+          var eventDate = $("<p>" + moment(eventsByLocation[i].datetime).format("MMMM D YYYY, h:mm A") + "</p>");
+          var eventLocation = $("<p>" + eventsByLocation[i].venue.city + " " + eventsByLocation[i].venue.region + " " + eventsByLocation[i].venue.country + "</p>");
+          var eventVenue = $("<p>" + eventsByLocation[i].venue.name + "</p>");
+          var eventURL = $("<a href='" + eventsByLocation[i].url + "' target='_blank'><p> Get tickets</p></a>");
+          var eventMap = $("<a href='https://www.google.com/maps/place/" + eventsByLocation[i].venue.latitude + "," + eventsByLocation[i].venue.longitude + "' target='_blank'><img src='https://maps.googleapis.com/maps/api/staticmap?center=" + eventsByLocation[i].venue.latitude + "," + eventsByLocation[i].venue.longitude + "&zoom=14&size=400x150&maptype=roadmap&markers=color:red%7Clabel:C%7C" + eventsByLocation[i].venue.latitude + "," + eventsByLocation[i].venue.longitude + "&key=AIzaSyDqItaMC7jRXipvTmfaH3bzfBiCxHNflr0'></a>");          
+          eventColumn1.append(eventDate, eventLocation, eventVenue, eventURL);
+          eventColumn2.append(eventMap);
+          eventDiv.append(eventColumn1, eventColumn2);
+          $("#events-holder").append(eventDiv);
+  
+          eventIndex++;
+        };
+        $("#events-holder").prepend("<button class='btn btn-danger' onclick='generateFiveMore()'>Show 5+</button>");
+  
+      } else {
+        $("#events-holder").empty();
+        vex.dialog.alert("No events in the selected location");
+      };
+    };
+  };
+
+function generateFiveMore() {
+  for (let i = eventIndex; i < (eventIndex + 5); i++) {
     
     var eventDiv = $("<div class='card d-flex flex-nowrap'>");
-    var eventColumn1 = $("<div class='col-lg-3'>");
-    var eventColumn2 = $("<div class='col-lg-3'>");
+    var eventColumn1 = $("<div class='col-lg-6'>");
+    var eventColumn2 = $("<div class='col-lg-6'>");
+    eventColumn1.append("<p>" + (i+1) + "</p>")
     var eventDate = $("<p>" + moment(events[0][i].datetime).format("MMMM D YYYY, h:mm A") + "</p>");
     var eventLocation = $("<p>" + events[0][i].venue.city + " " + events[0][i].venue.region + " " + events[0][i].venue.country + "</p>");
     var eventVenue = $("<p>" + events[0][i].venue.name + "</p>");
     var eventURL = $("<a href='" + events[0][i].url + "' target='_blank'><p> Get tickets</p></a>");
-
-    var eventMap = $("<img src='https://maps.googleapis.com/maps/api/staticmap?center=" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&zoom=14&size=400x150&maptype=roadmap&markers=color:red%7Clabel:C%7C" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&key=AIzaSyDqItaMC7jRXipvTmfaH3bzfBiCxHNflr0'>");
-    
-    // Appending the maps to each venue
-    
-
-
+    var eventMap = $("<a href='https://www.google.com/maps/place/" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "' target='_blank'><img src='https://maps.googleapis.com/maps/api/staticmap?center=" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&zoom=14&size=400x150&maptype=roadmap&markers=color:red%7Clabel:C%7C" + events[0][i].venue.latitude + "," + events[0][i].venue.longitude + "&key=AIzaSyDqItaMC7jRXipvTmfaH3bzfBiCxHNflr0'></a>");    
     eventColumn1.append(eventDate, eventLocation, eventVenue, eventURL);
     eventColumn2.append(eventMap);
     eventDiv.append(eventColumn1, eventColumn2);
     $("#events-holder").append(eventDiv);
   };
-
-    
-    
-
-    events.length = 0;
-    promiseEventsStatus = false;
-    promiseArtistStatus = false;
-};
+  eventIndex += 5;
+}
 
 $("#search-btn").on("click", function(event) {
     event.preventDefault();
     
     artistInput = $("#artist-input").val().trim();
     artistLetterArray = artistInput.split("");
-
-    if (artistInput === "") {
-      modal();
-    } else {
-      queryURLArtist = queryURLTemplate + artistInput + "?app_id=codingbootcamp";
-      queryURLEvents = queryURLTemplate + artistInput + "/events?app_id=codingbootcamp"
-      searchBandana();
-    };
 
     for (let i = 0; i < artistLetterArray.length; i++) {
 
@@ -232,17 +293,56 @@ $("#search-btn").on("click", function(event) {
     };
       
       
-    locationInput = $("#location-input").val().trim();
+    locationInput = ($("#location-input").val().trim()).toUpperCase();
     startDateInput = $("#start-date-input").val().trim();
     endDateInput = $("#end-date-input").val().trim();
     
     // added "Input" to the end of variable names
 
-    
-    
-    $("#artist-input").val("");
-    $("#location-input").val("");
-    $("#start-date-input").val("");
-    $("#end-date-input").val("");
+    if (artistInput === "") {
+      vex.dialog.alert("Please enter artist name");
+    } else if (startDateInput === "" && endDateInput !== "" || startDateInput !== "" && endDateInput === "") {
+      vex.dialog.alert("Please enter either both start and end dates or leave both blank");
+    } else {
+      queryURLArtist = queryURLTemplate + artistInput + "?app_id=codingbootcamp";
+      if (startDateInput === "" && endDateInput === "") {
+        queryURLEvents = queryURLTemplate + artistInput + "/events?app_id=codingbootcamp";
+      } else if (startDateInput !== "" && endDateInput !== "") {
+        queryURLEvents = queryURLTemplate + artistInput + "/events?date=" + moment(startDateInput).format("YYYY-MM-DD") + "," + moment(endDateInput).format("YYYY-MM-DD") + "&app_id=codingbootcamp";
+      }
+
+      searchBandana();
+      $("#artist-input").val("");
+      $("#location-input").val("");
+      $("#start-date-input").val("");
+      $("#end-date-input").val("");
+    };
 
   });
+
+  database.ref().limitToLast(5).on("child_added", function(generateRecentSearchCards) {  
+    var sv = generateRecentSearchCards.val();
+      artistName = sv.artistName;
+      artistImage = sv.artistImage;
+      function doTheThing(){
+      // add recent seach div with "recent-search"
+      var cardDiv = $("<div class='card' style='width: 10rem; position: relative'>");
+      var cardImg = $("<img class='card-img-top'>").attr("src", artistImage);
+      var cardBody = $("<div class='card-body d-flex justify-content-between' style='padding: 0.5rem; width: 100%; position: absolute; bottom: 0;'>");
+      // if (artistFB !== "") {
+      //   var cardFB = $("<a href='"+ artistFB +"' target='_blank'><img src='fb_logo.png' style='width: 30px'></a>");
+      // } else {
+      //   var cardFB = $("<div>");
+      // };
+      var cardName = $("<h6 class='card-title' style='margin: 0; font-weight: bold; background-color: rgba(255, 208, 0, 0.5); color: black; padding: 0.5rem 1rem; border-radius: 10px;'>").text(artistName);
+      // Append the new artist content
+      cardBody.append(cardName);
+      cardDiv.append(cardImg, cardBody);
+        $("#recent-search").append(cardDiv);
+        count ++;
+      };
+  // console.log(num.children) 
+  doTheThing();
+}, function(errorObject) {
+  console.log("Errors handled: " + errorObject.code);
+}); 
